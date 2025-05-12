f5746939786951d08cc663dec60eccbe72935662
Trung Do <dothanhtrung@pm.me>
2025-05-12 10:25:15 +0700

    Update example to use state-specific AutoTimerPlugin
    Switched from `AutoTimerPluginAnyState` to `AutoTimerPlugin` with state-specific configuration. Added `GameState` enum and initialized it to manage state transitions, ensuring the timer runs only in the `InGame` state.

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,21 +1,31 @@
 use bevy::log::info;
-use bevy::prelude::{App, Commands, Startup, Trigger};
+use bevy::prelude::{App, AppExtStates, Commands, NextState, ResMut, Startup, States, Trigger};
 use bevy::time::TimerMode;
 use bevy::DefaultPlugins;
-use bevy_auto_timer::{AutoTimer, AutoTimerFinished, AutoTimerPluginAnyState};
+use bevy_auto_timer::{AutoTimer, AutoTimerFinished, AutoTimerPlugin};
+
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+enum GameState {
+    #[default]
+    Menu,
+    InGame,
+}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(AutoTimerPluginAnyState::any())
+        .init_state::<GameState>()
+        .add_plugins(AutoTimerPlugin::new(vec![GameState::InGame]))
         .add_systems(Startup, setup)
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, mut next_state: ResMut<NextState<GameState>>) {
     commands
         .spawn(AutoTimer::from_seconds(1., TimerMode::Repeating))
         .observe(timeout);
+
+    next_state.set(GameState::InGame);
 }
 
 fn timeout(_: Trigger<AutoTimerFinished>) {
